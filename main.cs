cd959408f9992c80e49064502e59ae3911ac0d88
Paslolcal1 <25044435@student.hhs.nl>
2025-11-13 11:13:34 +0100

    effe opruimen

--- main.cs
+++ main.cs
@@ -1,9 +1,1 @@
 // Main File Software
-public class Pascal_test  {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-
-
-    }
-}
